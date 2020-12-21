fc8d0dd7dba686a45c89ef030a0fcb45c788a7ae
Femi Jemilohun <olufemi.jemilohun@gmail.com>
2020-12-20 19:57:21 -0500

    adding pytest framework to test api get/post requests

---
+++ tests/app_test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import sys
+# allow imports from directory
+sys.path.insert(0,'../api/app')
+
+from fastapi.testclient import TestClient
+from app import app
+from fastapi import HTTPException
+
+
+client = TestClient(app)
+
+
+def test_root():
+  '''Test API Get request in `../api/app/app.py`
+  '''
+  response = client.get('/api')
+  assert response.status_code == 200
+  assert response.json() == {'ping': 'pong'}
+
+
+def test_api():
+  '''Test the API Post request in `../api/app/app.py`
+  '''
+  # no route passed
+  response = client.post('/api', {})
+  assert response.status_code == 422 or response.status_code == 405
+  # data is not a valid json
+  response = client.post('/api', json={'route': 'ping', 'data': {}})
+  assert response.status_code == 200
